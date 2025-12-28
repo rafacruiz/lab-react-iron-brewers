6b74541d0791e2bd2e4634e1ff5e86e94d1d6763
Rafacruiz <rafacruiz@gmail.com>
2025-12-28 23:39:06 +0100

    feat: structure with services

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/ui";
 
-import { HomePage } from "./pages";
+import { AddBeerPage, AllBeersPage, BeerDetailsPage, HomePage, RandomBeerPage } from "./pages";
 
 function App() {
   return (
@@ -10,8 +10,11 @@ function App() {
     <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/beers" element={<AllBeersPage />} />
+        <Route path="/random-beer" element={<RandomBeerPage />} />
+        <Route path="/new-beer" element={<AddBeerPage />} />
+        <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
       </Routes>
     </div>
   );
--- src/components/beers/index.js
+++ src/components/beers/index.js
@@ -2,4 +2,4 @@ export { default as Home } from './beer-home/beer-home';
 export { default as AddBeer } from './beer-add/beer-add';
 export { default as RandomBeer } from './beer-Random/beer-random';
 export { default as BeerDetails } from './beer-details/beer-details';
-export { default as AllBeers } from './beer-all/beer-all';
+export { default as AllBeers } from './beer-all/beers-all';
